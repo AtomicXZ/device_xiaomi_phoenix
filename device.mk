a0a0881fe6f05e4eecedd074d8cfa1be9d3aac8c
Peter Cai <peter@typeblog.net>
2020-10-02 18:07:17 +0530

    phoenix: devicesettings: allow switching notch style
    * Some may actually like the non-filled punchhole * 2 notch.
    
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- device.mk
+++ device.mk
@@ -106,6 +106,10 @@ PRODUCT_PACKAGES += \
     SecureElement \
     Tag
 
+# Notch style overlay
+PRODUCT_PACKAGES += \
+    NotchNoFillOverlay
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
